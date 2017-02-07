Linaro/zephyr

commit 6b013c4721c50c5f9c6ae628dc95f13ff3012ba3

net: nbuf: Add timeout to net_buf getters
This commit changes the net_buf getter functions in nbuf.h
by adding a timeout parameter. These function prototypes
are changed to accept a timeout parameter.
	net_nbuf_get_rx()
	net_nbuf_get_tx()
	net_nbuf_get_data()
	net_nbuf_get_reserve_rx()
	net_nbuf_get_reserve_tx()
	net_nbuf_get_reserve_data()
	net_nbuf_copy()
	net_nbuf_copy_all()
	net_nbuf_push()
	net_nbuf_append()
	net_nbuf_write()
	net_nbuf_insert()

Following convinience functions have not been changed
	net_nbuf_append_u8
	net_nbuf_append_be16
	net_nbuf_append_be32
	net_nbuf_insert_u8
	net_nbuf_insert_be16
	net_nbuf_insert_be32
	net_nbuf_write_u8
	net_nbuf_write_be16
	net_nbuf_write_be32
so they call the base function using K_FOREVER. Use the
base function if you want to have a timeout when net_buf
is allocated.

Change-Id: I20bb602ffb73069e5a02668fce60575141586c0f
Signed-off-by: Jukka Rissanen <jukka.rissanen@linux.intel.com>

samples/net/irc_bot/src/irc-bot.c
@@ -146,12 +146,12 @@ transmit(struct net_context *ctx, char buffer[], size_t len)
 {
 	struct net_buf *send_buf;
 
-	send_buf = net_nbuf_get_tx(ctx);
+	send_buf = net_nbuf_get_tx(ctx, K_FOREVER);
 	if (!send_buf) {
 		return -ENOMEM;
 	}
 
-	if (!net_nbuf_append(send_buf, len, buffer)) {
+	if (!net_nbuf_append(send_buf, len, buffer), K_FOREVER) {
 		return -EINVAL;
 	}
 

doc/subsystems/networking/connectivity-example-app.c
@@ -95,7 +95,7 @@ static struct net_buf *udp_recv(const char *name,
 	NET_INFO("%s received %u bytes", name,
 	      net_nbuf_appdatalen(buf));
 
-	reply_buf = net_nbuf_get_tx(context);
+	reply_buf = net_nbuf_get_tx(context, K_FOREVER);
 
 	NET_ASSERT(reply_buf);
 
@@ -113,7 +113,7 @@ static struct net_buf *udp_recv(const char *name,
 	net_buf_pull(tmp, header_len);
 
 	while (tmp) {
-		frag = net_nbuf_get_data(context);
+		frag = net_nbuf_get_data(context, K_FOREVER);
 
 		memcpy(net_buf_add(frag, tmp->len), tmp->data, tmp->len);
 

drivers/console/telnet_console.c
@@ -125,7 +125,7 @@ static void telnet_end_client_connection(void)
 
 static int telnet_setup_out_buf(struct net_context *client)
 {
-	out_buf = net_nbuf_get_tx(client);
+	out_buf = net_nbuf_get_tx(client, K_FOREVER);
 	if (!out_buf) {
 		/* Cannot happen atm, nbuf waits indefinitely */
 		return -ENOBUFS;
@@ -234,7 +234,7 @@ static inline bool telnet_send(void)
 	struct line_buf *lb = telnet_rb_get_line_out();
 
 	if (lb) {
-		net_nbuf_append(out_buf, lb->len, lb->buf);
+		net_nbuf_append(out_buf, lb->len, lb->buf, K_FOREVER);
 
 		/* We reinitialize the line buffer */
 		lb->len = 0;
@@ -258,7 +258,7 @@ static int telnet_console_out_nothing(int c)
 
 static inline void telnet_command_send_reply(uint8_t *msg, uint16_t len)
 {
-	net_nbuf_append(out_buf, len, msg);
+	net_nbuf_append(out_buf, len, msg, K_FOREVER);
 
 	net_context_send(out_buf, telnet_sent_cb,
 			 K_NO_WAIT, NULL, NULL);

drivers/ethernet/eth_enc28j60.c
@@ -551,7 +551,7 @@ static int eth_enc28j60_rx(struct device *dev)
 		lengthfr = frm_len;
 
 		/* Get the frame from the buffer */
-		buf = net_nbuf_get_reserve_rx(0);
+		buf = net_nbuf_get_reserve_rx(0, K_NO_WAIT);
 		if (!buf) {
 			SYS_LOG_ERR("Could not allocate rx buffer");
 			goto done;
@@ -565,7 +565,7 @@ static int eth_enc28j60_rx(struct device *dev)
 			size_t spi_frame_len;
 
 			/* Reserve a data frag to receive the frame */
-			pkt_buf = net_nbuf_get_reserve_data(0);
+			pkt_buf = net_nbuf_get_reserve_data(0, K_NO_WAIT);
 			if (!pkt_buf) {
 				SYS_LOG_ERR("Could not allocate data buffer");
 				net_buf_unref(buf);

drivers/ethernet/eth_mcux.c
@@ -145,7 +145,7 @@ static void eth_rx(struct device *iface)
 		return;
 	}
 
-	buf = net_nbuf_get_reserve_rx(0);
+	buf = net_nbuf_get_reserve_rx(0, K_NO_WAIT);
 	if (!buf) {
 		/* We failed to get a receive buffer.  We don't add
 		 * any further logging here because the allocator
@@ -188,7 +188,7 @@ static void eth_rx(struct device *iface)
 		struct net_buf *pkt_buf;
 		size_t frag_len;
 
-		pkt_buf = net_nbuf_get_reserve_data(0);
+		pkt_buf = net_nbuf_get_reserve_data(0, K_NO_WAIT);
 		if (!pkt_buf) {
 			irq_unlock(imask);
 			SYS_LOG_ERR("Failed to get fragment buf\n");

drivers/ethernet/eth_sam_gmac.c
@@ -162,7 +162,7 @@ static int rx_descriptors_init(Gmac *gmac, struct gmac_queue *queue)
 	rx_nbuf_list->tail = 0;
 
 	for (int i = 0; i < rx_desc_list->len; i++) {
-		rx_buf = net_nbuf_get_reserve_data(0);
+		rx_buf = net_nbuf_get_reserve_data(0, K_NO_WAIT);
 		if (rx_buf == NULL) {
 			free_rx_bufs(rx_nbuf_list);
 			SYS_LOG_ERR("Failed to reserve data net buffers");
@@ -470,7 +470,7 @@ static struct net_buf *frame_get(struct gmac_queue *queue)
 		return NULL;
 	}
 
-	rx_frame = net_nbuf_get_reserve_rx(0);
+	rx_frame = net_nbuf_get_reserve_rx(0, K_NO_WAIT);
 
 	/* Process a frame */
 	prev_frag = rx_frame;
@@ -509,7 +509,7 @@ static struct net_buf *frame_get(struct gmac_queue *queue)
 			DCACHE_INVALIDATE(frag_data, frag_len);
 
 			/* Get a new data net buffer from the buffer pool */
-			new_frag = net_nbuf_get_reserve_data(0);
+			new_frag = net_nbuf_get_reserve_data(0, K_NO_WAIT);
 			if (new_frag == NULL) {
 				queue->err_rx_frames_dropped++;
 				net_buf_unref(rx_frame);

drivers/ieee802154/ieee802154_cc2520.c
@@ -594,7 +594,7 @@ static void cc2520_rx(int arg)
 			goto flush;
 		}
 
-		buf = net_nbuf_get_reserve_rx(0);
+		buf = net_nbuf_get_reserve_rx(0, K_NO_WAIT);
 		if (!buf) {
 			SYS_LOG_ERR("No buf available");
 			goto flush;
@@ -604,9 +604,9 @@ static void cc2520_rx(int arg)
 		/**
 		 * Reserve 1 byte for length
 		 */
-		pkt_buf = net_nbuf_get_reserve_data(1);
+		pkt_buf = net_nbuf_get_reserve_data(1, K_NO_WAIT);
 #else
-		pkt_buf = net_nbuf_get_reserve_data(0);
+		pkt_buf = net_nbuf_get_reserve_data(0, K_NO_WAIT);
 #endif
 		if (!pkt_buf) {
 			SYS_LOG_ERR("No pkt_buf available");

drivers/ieee802154/ieee802154_mcr20a.c
@@ -524,7 +524,7 @@ static inline void mcr20a_rx(struct mcr20a_context *mcr20a)
 	pkt_len = read_reg_rx_frm_len(&mcr20a->spi);
 	pkt_len -= MCR20A_FCS_LENGTH;
 
-	buf = net_nbuf_get_reserve_rx(0);
+	buf = net_nbuf_get_reserve_rx(0, K_NO_WAIT);
 	if (!buf) {
 		SYS_LOG_ERR("No buf available");
 		goto out;
@@ -535,9 +535,9 @@ static inline void mcr20a_rx(struct mcr20a_context *mcr20a)
 	/**
 	 * Reserve 1 byte for length
 	 */
-	pkt_buf = net_nbuf_get_reserve_data(1);
+	pkt_buf = net_nbuf_get_reserve_data(1, K_NO_WAIT);
 #else
-	pkt_buf = net_nbuf_get_reserve_data(0);
+	pkt_buf = net_nbuf_get_reserve_data(0, K_NO_WAIT);
 #endif
 	if (!pkt_buf) {
 		SYS_LOG_ERR("No pkt_buf available");

commit 07baba210b8dc158f1f5995a15defc61d8e5ed0f

samples: net: irc_bot: fix build break
Fix build break introduced in commit 6b013c4721c5
("net: nbuf: Add timeout to net_buf getters")

Change-Id: Iaa521c6a49ae740eaee5276936442ab8aa4c47ba
Signed-off-by: Michael Scott <michael.scott@linaro.org>

samples/net/irc_bot/src/irc-bot.c
@@ -151,7 +151,7 @@ transmit(struct net_context *ctx, char buffer[], size_t len)
 		return -ENOMEM;
 	}
 
-	if (!net_nbuf_append(send_buf, len, buffer), K_FOREVER) {
+	if (!net_nbuf_append(send_buf, len, buffer, K_FOREVER)) {
 		return -EINVAL;
 	}
 